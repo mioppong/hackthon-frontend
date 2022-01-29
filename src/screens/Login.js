489fdcdb642f0283ec7598e22423d1731c2be8cf
oppong <michaeloppong123@gmail.com>
2022-01-29 17:36:15 -0500

    basic login screen

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,15 +7,34 @@ const Login = () => {
   const handleLogin = () => {
     navigate("/home");
   };
+
+  const container = {
+    backgroundColor: "red",
+    display: "flex",
+    height: "100vh",
+    width: "100vw",
+  };
   return (
-    <div>
-      <div>LOGIN</div>
-      <div>LOGIN</div>
-      <div>LOGIN</div>
-      <div>LOGIN</div>
-      <div>LOGIN</div>
-      <div>LOGIN</div>
-      <Button variant="contained" title="Login" onClick={handleLogin} />
+    <div style={container}>
+      {/* left side */}
+      <div style={{ height: "100vh", backgroundColor: "blue", width: "50vw" }}>
+        <Button
+          variant="contained"
+          title="Login"
+          style={{ height: 50, width: 50, backgroundColor: "yellow" }}
+          onClick={handleLogin}
+        />
+      </div>
+      
+      {/*RIGHT SIDE  */}
+      <div style={{ height: "100vh", backgroundColor: "green", width: "50vw" }}>
+        <Button
+          variant="contained"
+          title="Login"
+          style={{ height: 50, width: 50, backgroundColor: "yellow" }}
+          onClick={handleLogin}
+        />
+      </div>
     </div>
   );
 };
